9a4dc1c0bd5344a6f76aa03c9edf7faa3cd0d98d
Hyo Byun <sublimice@gmail.com>
2019-01-09 22:40:04 -0500

    Added dat.gui

---
+++ volsurface.js
@@ -0,0 +1,53 @@
+var camera, scene, renderer;
+var geometry, material, mesh;
+
+var surfaceState={
+  'ui': {
+    "addFile" :function(){ console.log("clicked") },
+    "calls" :true,
+    "puts"  : true
+  }
+};
+
+init();
+animate();
+createUI();
+
+function init() {
+
+	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera.position.z = 1;
+
+	scene = new THREE.Scene();
+
+	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+	material = new THREE.MeshNormalMaterial();
+
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.02;
+
+	renderer.render( scene, camera );
+
+}
+
+function createUI() {
+  var gui = new dat.GUI();
+
+  gui.add(surfaceState.ui, 'addFile').name('AddFile');
+
+  gui.add(surfaceState.ui, 'calls');
+  gui.add(surfaceState.ui, 'puts');
+}
